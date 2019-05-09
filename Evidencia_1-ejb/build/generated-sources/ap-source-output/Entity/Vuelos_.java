7ea6119be0ebe9d652a55d1b7c7f8372d1aad491
Arturo <arturo.vazquez.1730@gmail.com>
2019-05-09 12:07:28 -0500

    Add primary Faces

--- Evidencia_1-ejb/build/generated-sources/ap-source-output/Entity/Vuelos_.java
+++ Evidencia_1-ejb/build/generated-sources/ap-source-output/Entity/Vuelos_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-02T11:35:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-09T11:34:16")
 @StaticMetamodel(Vuelos.class)
 public class Vuelos_ { 
 
